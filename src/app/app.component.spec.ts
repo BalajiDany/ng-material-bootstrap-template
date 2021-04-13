a86fbae9219fb24629c2c88d822ebecfe615ed9e
Balaji <d.balaji.mc@gmail.com>
2021-04-13 21:17:23 +0530

    Cleaned Boiler plate codes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'ng-material-bootstrap-template'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('ng-material-bootstrap-template');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('ng-material-bootstrap-template app is running!');
-    });
 });
