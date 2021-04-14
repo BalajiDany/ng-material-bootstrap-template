57553895953e097072abddef964058bccaecb87f
Balaji <d.balaji.mc@gmail.com>
2021-04-14 12:51:54 +0530

    Resolving dependency for TranslocoHttpService.spec

--- src/app/@core/transloco/transloco-http-loader.service.spec.ts
+++ src/app/@core/transloco/transloco-http-loader.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { TranslocoHttpLoaderService } from './transloco-http-loader.service';
@@ -6,7 +7,9 @@ describe('TranslocoHttpLoaderService', () => {
     let service: TranslocoHttpLoaderService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule]
+        });
         service = TestBed.inject(TranslocoHttpLoaderService);
     });
 
