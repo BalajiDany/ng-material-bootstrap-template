6e55d1155c2ca9115a14db79e19ce2aebdda6201
Balaji <d.balaji.mc@gmail.com>
2021-04-22 17:37:37 +0530

    Redirect empty path to sign in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    // TODO: UPDATE_ME Change this to default home path
+    // Since Home page is not developed, redirecting to Sign In page.
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'entry/signin',
+    },
     {
         path: 'entry',
         loadChildren: () => import('./features/entry/entry.module')
