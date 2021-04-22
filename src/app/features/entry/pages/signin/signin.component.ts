6aae1a4688df6f5134761389d5bdeccf4bb2a14b
Balaji <d.balaji.mc@gmail.com>
2021-04-22 14:37:12 +0530

    Unused Imports

--- src/app/features/entry/pages/signin/signin.component.ts
+++ src/app/features/entry/pages/signin/signin.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { LocaleManagerService } from 'src/app/@core/services/locale-manager.service';
-import { EntryComponent } from '../../entry.component';
 
 @Component({
     selector: 'app-signin',
