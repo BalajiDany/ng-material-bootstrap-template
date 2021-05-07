adf5bddad7f48552ac61d54b81ea823dd9e033e2
Balaji <d.balaji.mc@gmail.com>
2021-05-07 11:08:08 +0530

    Formated navigation service manager

--- src/app/@core/services/navigation-manager.service.spec.ts
+++ src/app/@core/services/navigation-manager.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { NavigationManagerService } from './navigation-manager.service';
 
 describe('NavigationManagerService', () => {
-  let service: NavigationManagerService;
+    let service: NavigationManagerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NavigationManagerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(NavigationManagerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
--- src/app/@core/services/navigation-manager.service.ts
+++ src/app/@core/services/navigation-manager.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NavigationManagerService {
 
